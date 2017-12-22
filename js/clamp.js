7417763cb7a90b36a441aa38c39840a8198934f9
Andrew Nguyen <amnguyen1@mail.usf.edu>
2017-12-21 19:13:36 -0500

    created blog and about page

---
+++ js/clamp.js
@@ -0,0 +1,7 @@
+var p=$('.post_preview_text p');
+var divh=$('.post_preview_text').height();
+while ($(p).outerHeight()>divh) {
+    $(p).text(function (index, text) {
+        return text.replace(/\W*\s(\S)*$/, '...');
+    });
+}
